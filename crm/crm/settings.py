899950a159a5289d89d5c113d614397617fd2b45
pranjal <pranjaldeepkanel@gmail.com>
2020-10-08 15:19:04 +0545

    Installed wsgi

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
