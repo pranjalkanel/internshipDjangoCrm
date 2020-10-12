a8da501d1c1c55e51d0ff9bcadcf383ed57ea3ba
pranjal <pranjaldeepkanel@gmail.com>
2020-10-12 13:58:00 +0545

    Stable version on localhostx

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Demo_test',
-        'USER': 'postgres',
-        'PASSWORD': os.environ.get('DB'), 
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
