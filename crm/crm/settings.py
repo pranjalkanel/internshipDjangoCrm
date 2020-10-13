7a1024f2a798cb57036ecc2ff69d10103bb8d619
pranjal <pranjaldeepkanel@gmail.com>
2020-10-13 14:36:48 +0545

    Added files for deployment
    Procfile, requirement.txt and run-time.txt have been created in the root directory of the application. Refer to documentation or Dennis Ivy's tutorial.
    These files should always be in the root directory of the application to be deployed in heroku.
    
    In settings.py,
    	Debug has been set to false. This is necessary if intended to deploy.
    	Middleware for whitenoise has been added in the second line. (currently giving no dir error which is ingnorable for now)
    	Allowed hosts are set to localhost and heroku app
    	Static files root has been aded on top of static url

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('DjangoKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://internship-django-crm.herokuapp.com/', '127.0.0.1', 'localhost',]
 
 
 # Application definition
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 
